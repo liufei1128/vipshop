57d8c5e5c4627a23eb6aa70fd8ae5bafddb869b0
qinqin_007 <1796696908@qq.com>
2018-12-13 11:58:25 +0800

    完成首页搜索框跳转

---
+++ app/static/js/search-input.js
@@ -0,0 +1,64 @@
+
+$('.text').on("click", function () {
+    $('.search_onclick').css("display", "block");
+})
+$('.text').on("blur", function () {
+    $('.search_onclick').css("display", "none");
+})
+const searchInp = (function () {
+    return {
+        init(ele) {
+            this.$search = document.querySelector(ele);
+            this.$searchInp = this.$search.querySelector('.text');
+            this.$searchRes = this.$search.querySelector('.search_result');
+            this.$searchOnclick = this.$search.querySelector('.search_onclick')
+            this.event();
+        },
+        event() {
+            const _this = this;
+            const $inp = this.$searchInp;
+            $inp.oninput = function () {
+                if(this.value != ''){
+                    _this.$searchRes.style.display = 'block';
+                    _this.$searchOnclick.style.display = 'none';
+                    _this.getData(this.value);
+                } else {
+                    _this.$searchRes.style.display = 'none';
+                    _this.$searchOnclick.style.display = 'block';
+                }
+            }
+            $inp.onfocus = function () {
+                if (this.value != '') {
+                    _this.$searchRes.style.display = 'block';
+                    _this.$searchOnclick.style.display = 'none';
+                }
+            }
+            $inp.onblur = function () {
+                _this.$searchRes.style.display = 'none';
+            }
+        },
+        getData(val) {
+            const _this = this;
+            sendJsonp('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su',{
+                wd: val,
+                cb: "insertData"
+            }
+        )
+        },
+        insertData(data) {
+            data = data.s;
+            this.$searchRes.innerHTML = '';
+            for(let i = 0; i < data.length; i++){
+                let $li = document.createElement('li');
+                this.$searchRes.appendChild($li);
+                $li.innerHTML = data[i];
+            }
+            this.$searchRes.style.display = 'block';
+            $li.onclick = function(){
+                location.assign()
+            }
+        }
+    }
+}())
+searchInp.init('.search');
+var insertData = searchInp.insertData.bind(searchInp);
