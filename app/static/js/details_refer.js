fff8f8507bb4876e61e6a2c0344fe37adce4c7fd
Vichede91 <851517199@qq.com>
2018-12-13 20:52:49 +0800

    修改shop_car的bug，新增购物车添加多个物品功能

--- app/static/js/details_refer.js
+++ app/static/js/details_refer.js
@@ -7,12 +7,14 @@ var $btnall = $('.button_box button');
 var $count = $('.button_box i');
 var $join_shop = $('.join_shop');
 var $price = $('.price_left i');
+var $title =$('.commodity_content h3');
+var $imgSrc = $('.imglist_bigbox li img');
 var t=1;
 var data ={
     "id":"1",
-    "title": "VOA雪白貂皮毛帽领钻石扣蕃茄红绗缝牡丹绣花两件套真丝棉衣羽绒服",
+    "title": "",
     "price": 0,
-    "imgsrc": "http://a.vpimg3.com/upload/merchandise/pdcvis/113684/2018/1116/79/b5c7b872-7576-45a0-a8a5-f7809f5e40a1.jpg",
+    "imgsrc": "",
     "count": 0,
     "size": "M"
 };
@@ -36,11 +38,12 @@ var data ={
     });
     $($join_shop).click(function(){
         var count = $($count).text(),
-        price = $($price).text();
+        price = $($price).text().substring(1);
         console.log(count,price);
         data.count =  Number(count);
-        console.log(data.count,Number(data.count),Number(count));
-        data.price = price;
+        data.price = Number(price);
+        data.title = $($title).text();
+        data.imgsrc = $($imgSrc).attr('src');
         setItem(data);
     });
     var setItem =function(data) {
@@ -49,7 +52,7 @@ var data ={
         shopList = JSON.parse(shopList);
         // 在把新数据push到原有数据
         for(var i =0;i<shopList.length;i++){
-            if(data.id == shopList[i].id){
+            if(data.title == shopList[i].title){
                 shopList[i].count += data.count;
                 break;
             }
